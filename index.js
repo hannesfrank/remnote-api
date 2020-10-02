6a49d99f1dba4e80a4d836953892dc8d8889deeb
Hannes Frank <frankhannes@gmail.com>
2020-10-03 00:14:07 +0200

    Add .prettierrc and util.js

--- index.js
+++ index.js
@@ -146,104 +146,21 @@ class RemNoteAPIV0 {
   receiveMessage(event) {
     const data = event.data;
     const messageId = data.messageId;
-    this.messagePromises[messageId](data.response);
-    delete this.messagePromises[messageId];
+    // FIXME: There is an issue again that the same message is received twice. I had this problem before
+    // and last time I tinkered a bit with the instantiation of the API, because RemNoteAPIV0 needs
+    // to be a singleton.
+    // I don't know why im getting this again. The console.warn below shows that there is only one
+    // instance. I just manually check if there is a message for now to suppress the warning.
+    if (messageId in this.messagePromises) {
+      this.messagePromises[messageId](data.response);
+      delete this.messagePromises[messageId];
+    }
   }
 }
 
+console.warn('Evaluating RemNoteAPI');
 const RemNoteAPI = {
   v0: new RemNoteAPIV0(),
 };
 
 export default RemNoteAPI;
-
-const SETTINGS_REM = 'Plugin Settings';
-
-export async function getDocument() {
-  const context = await RemNoteAPI.v0.get_context();
-  console.log('context', context);
-  const documentRem = await RemNoteAPI.v0.get(context.documentId);
-  return documentRem;
-}
-
-export async function getRem(options = {}) {
-  if (options.id) {
-    return await RemNoteAPI.v0.get(options.id);
-  } else if (options.name) {
-    return await RemNoteAPI.v0.get_by_name(options.name);
-  }
-}
-
-export async function getChildren(rem, visibleOnly = False) {
-  const children = visibleOnly ? rem.visibleRemOnDocument : rem.children;
-  // TODO: Children have the correct order, visibleRemOnDocument don't
-  console.log('Children', children);
-  children.reverse();
-  return await Promise.all(children.map((remId) => RemNoteAPI.v0.get(remId)));
-}
-
-export async function getVisibleChildren(remId) {
-  return getChildren(remId, true);
-}
-
-/**
- * Take a Rem, and extract its text. The rem.name and rem.content fields are
- * both of type "RichTextInterface", which is an array of text strings, or js
- * objects representing rich text element. Text is extracted recursively from
- * Rem Reference elements.
- */
-export async function getRemText(rem, exploredRem = []) {
-  if (!rem.found) return '';
-
-  const richTextElementsText = await Promise.all(
-    // Go through each element in the rich text
-    rem.name.concat(rem.content || []).map(async (richTextElement) => {
-      // If the element is a string, juts return it
-      if (typeof richTextElement == 'string') {
-        return richTextElement;
-        // If the element is a Rem Reference (i == "q"), then recursively get that Rem Reference's text.
-      } else if (richTextElement.i == 'q' && !exploredRem.includes(richTextElement._id)) {
-        return await getRemText(
-          await RemNoteAPI.v0.get(richTextElement._id),
-          // Track explored Rem to avoid infinite loops
-          exploredRem.concat([richTextElement._id])
-        );
-      } else {
-        // If the Rem is some other rich text element, just take its .text property.
-        return richTextElement.text;
-      }
-    })
-  );
-  return richTextElementsText.join('');
-}
-
-export async function loadText(remList) {
-  await Promise.all(
-    remList.map(async (rem) => {
-      rem.text = await getRemText(rem);
-      return rem;
-    })
-  );
-}
-
-export async function loadTags(rem) {
-  rem.tags = await Promise.all(
-    rem.tagParents.map(async (tagId) => {
-      let tagRem = await RemNoteAPI.v0.get(tagId);
-      return tagRem.nameAsMarkdown;
-      // let text = await getRemText(tagRem);
-      // console.log(text);
-      // return text;
-    })
-  );
-}
-
-/** ----------- Plugin related --------------- */
-
-/**
- * @returns URL parameters as an Object. When supplied with duplicate keys, only
- *          the last value is taken
- */
-export function getURLConfig() {
-  return Object.fromEntries(new URLSearchParams(location.search));
-}
---
+++ util.js
@@ -0,0 +1,117 @@
+import RemNoteAPI from './index';
+
+export async function getContext() {
+  const context = await RemNoteAPI.v0.get_context();
+  // TODO: transparent preloading
+  context.rem = await getRem({ id: context.remId });
+  context.document = await getRem({ id: context.documentId });
+  return context;
+}
+
+export async function getDocument() {
+  const context = await RemNoteAPI.v0.get_context();
+  const documentRem = RemNoteAPI.v0.get(context.documentId);
+  return documentRem;
+}
+
+export async function getRem(options = {}) {
+  if (options.id) {
+    return RemNoteAPI.v0.get(options.id);
+  } else if (options.name) {
+    return RemNoteAPI.v0.get_by_name(options.name);
+  }
+}
+
+export async function getChildren(rem, visibleOnly = False) {
+  const children = visibleOnly ? rem.visibleRemOnDocument : rem.children;
+  // TODO: Children have the correct order, visibleRemOnDocument don't
+  console.log('Children', children);
+  children.reverse();
+  return Promise.all(children.map((remId) => RemNoteAPI.v0.get(remId)));
+}
+
+/**
+ * Insert an image to a rem.
+ * @param {*} rem Rem or RemID 
+ */
+export async function insertImage(rem, imageURL) {
+  if (typeof rem === 'string') {
+    rem = await getRem({id: rem});
+  }
+  // append
+  console.log('remnote append', rem);
+  if (rem.content) {
+    await RemNoteAPI.v0.update(rem._id, {
+      content: rem.contentAsMarkdown + `![](${imageURL})`
+    });
+  } else {
+    await RemNoteAPI.v0.update(rem._id, { name: rem.nameAsMarkdown + `![](${imageURL})` });
+  }
+}
+  
+
+export async function getVisibleChildren(remId) {
+  return getChildren(remId, true);
+}
+
+/**
+ * Take a Rem, and extract its text. The rem.name and rem.content fields are
+ * both of type "RichTextInterface", which is an array of text strings, or js
+ * objects representing rich text element. Text is extracted recursively from
+ * Rem Reference elements.
+ */
+export async function getRemText(rem, exploredRem = []) {
+  if (!rem.found) return '';
+
+  const richTextElementsText = await Promise.all(
+    // Go through each element in the rich text
+    rem.name.concat(rem.content || []).map(async (richTextElement) => {
+      // If the element is a string, juts return it
+      if (typeof richTextElement == 'string') {
+        return richTextElement;
+        // If the element is a Rem Reference (i == "q"), then recursively get that Rem Reference's text.
+      } else if (richTextElement.i == 'q' && !exploredRem.includes(richTextElement._id)) {
+        return await getRemText(
+          await RemNoteAPI.v0.get(richTextElement._id),
+          // Track explored Rem to avoid infinite loops
+          exploredRem.concat([richTextElement._id])
+        );
+      } else {
+        // If the Rem is some other rich text element, just take its .text property.
+        return richTextElement.text;
+      }
+    })
+  );
+  return richTextElementsText.join('');
+}
+
+export async function loadText(remList) {
+  await Promise.all(
+    remList.map(async (rem) => {
+      rem.text = await getRemText(rem);
+      return rem;
+    })
+  );
+}
+
+export async function loadTags(rem) {
+  rem.tags = await Promise.all(
+    rem.tagParents.map(async (tagId) => {
+      let tagRem = await RemNoteAPI.v0.get(tagId);
+      return tagRem.nameAsMarkdown;
+      // let text = await getRemText(tagRem);
+      // console.log(text);
+      // return text;
+    })
+  );
+}
+
+/** ----------- Plugin related --------------- */
+
+/**
+ * @returns URL parameters as an Object. When supplied with duplicate keys, only
+ *          the last value is taken
+ */
+export function getURLConfig() {
+  return Object.fromEntries(new URLSearchParams(location.search));
+}
